0fa1bc39c4f8565d33ffdf514d14344788484b14
Kenny Tordeur <kenny.tordeur@outlook.com>
2016-01-27 14:28:08 +0100

    sprint1 - setup build system & browserify

---
+++ src/main.js
@@ -0,0 +1,18 @@
+require("./kern/kern.js");
+require("./framework/wl.js");
+
+/* data objects*/
+require("./framework/defaults.js");
+require("./framework/cobjdata.js");
+require("./framework/cgroupdata.js");
+require("./framework/framedata.js");
+require("./framework/layerdata.js");
+require("./framework/stagedata.js");
+
+/* view objects*/
+require("./framework/cobjview.js");
+require("./framework/cgroupview.js");
+
+/* others*/
+require("./framework/pluginmanager.js");
+require("./framework/repository.js");
