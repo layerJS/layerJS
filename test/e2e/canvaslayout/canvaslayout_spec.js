ef7deb2ebbf84833a96bb2b8252ab245df0704af
ktordeur <kenny.tordeur@outlook.com>
2016-04-05 08:15:42 +0200

    e2e Tests fitting functions (fitting functions tests)

--- test/e2e/canvaslayout/canvaslayout_spec.js
+++ test/e2e/canvaslayout/canvaslayout_spec.js
@@ -24,66 +24,64 @@ describe("CanvasLayout", function() {
 
   it('will transition to a frame and will apply a transform on all frames within the layer', function() {
     browser.get('canvasLayout/canvaslayout.html').then(function() {
-      browser.sleep(3000).then(function() {
-        var stage = element(by.id('root'));
-        var layer1 = element(by.id('layer1'));
-        var f1 = element(by.id('main'));
-        var f2 = element(by.id('second'));
-        var f3 = element(by.id('thirth'));
+      var stage = element(by.id('root'));
+      var layer1 = element(by.id('layer1'));
+      var f1 = element(by.id('main'));
+      var f2 = element(by.id('second'));
+      var f3 = element(by.id('thirth'));
 
-        expect(f1.getCssValue('display')).toBe('block');
-        expect(f2.getCssValue('display')).toBe('block');
-        expect(f3.getCssValue('display')).toBe('block');
+      expect(f1.getCssValue('display')).toBe('block');
+      expect(f2.getCssValue('display')).toBe('block');
+      expect(f3.getCssValue('display')).toBe('block');
 
-        utilities.transitionTo('layer1', 'second', {}).then(function() {
+      utilities.transitionTo('layer1', 'second', {}).then(function() {
 
-          protractor.promise.all([
-            utilities.getBoundingClientRect('root'),
-            f1.getCssValue('display'),
-            utilities.getBoundingClientRect('main'),
-            utilities.getScale('main'),
-            utilities.getRotation('main'),
-            f2.getCssValue('display'),
-            utilities.getBoundingClientRect('second'),
-            utilities.getScale('second'),
-            utilities.getRotation('second'),
-            f3.getCssValue('display'),
-            utilities.getBoundingClientRect('thirth'),
-            utilities.getScale('thirth'),
-            utilities.getRotation('thirth')
-          ]).then(function(data) {
+        protractor.promise.all([
+          utilities.getBoundingClientRect('root'),
+          f1.getCssValue('display'),
+          utilities.getBoundingClientRect('main'),
+          utilities.getScale('main'),
+          utilities.getRotation('main'),
+          f2.getCssValue('display'),
+          utilities.getBoundingClientRect('second'),
+          utilities.getScale('second'),
+          utilities.getRotation('second'),
+          f3.getCssValue('display'),
+          utilities.getBoundingClientRect('thirth'),
+          utilities.getScale('thirth'),
+          utilities.getRotation('thirth')
+        ]).then(function(data) {
 
-            var stage_dimensions = data[0];
-            var f1_display = data[1];
-            var f1_dimensions = data[2];
-            var f1_scale = data[3];
-            var f1_rotation = data[4];
-            var f2_display = data[5];
-            var f2_dimensions = data[6];
-            var f2_scale = data[7];
-            var f2_rotation = data[8];
-            var f3_display = data[9];
-            var f3_dimensions = data[10];
-            var f3_scale = data[11];
-            var f3_rotation = data[12];
+          var stage_dimensions = data[0];
+          var f1_display = data[1];
+          var f1_dimensions = data[2];
+          var f1_scale = data[3];
+          var f1_rotation = data[4];
+          var f2_display = data[5];
+          var f2_dimensions = data[6];
+          var f2_scale = data[7];
+          var f2_rotation = data[8];
+          var f3_display = data[9];
+          var f3_dimensions = data[10];
+          var f3_scale = data[11];
+          var f3_rotation = data[12];
 
-            expect(f1_display).toBe('block');
-            expect(f2_display).toBe('block');
-            expect(f3_display).toBe('block');
-            expect(stage_dimensions.width).toBe(f2_dimensions.width);
-            expect(f1_dimensions.left).toBeWithinRange(-273, -272);
-            expect(f1_dimensions.top).toBeWithinRange(-55, -54);
-            expect(f1_scale).toBeWithinRange(0.544, 0.546);
-            expect(f1_rotation).toBe(0);
-            expect(f2_dimensions.left).toBeWithinRange(0, 0.5);
-            expect(f2_dimensions.top).toBeWithinRange(0, 0.5);
-            expect(f2_scale).toBeWithinRange(0.544, 0.546);
-            expect(f2_rotation).toBe(0);
-            expect(f3_dimensions.left).toBeWithinRange(-96, -93);
-            expect(f3_dimensions.top).toBeWithinRange(-50, -49);
-            expect(f3_scale).toBeWithinRange(0.272, 0.273);
-            expect(f3_rotation).toBe(30);
-          });
+          expect(f1_display).toBe('block');
+          expect(f2_display).toBe('block');
+          expect(f3_display).toBe('block');
+          expect(stage_dimensions.width).toBe(f2_dimensions.width);
+          expect(f1_dimensions.left).toBeWithinRange(-273, -272);
+          expect(f1_dimensions.top).toBeWithinRange(-55, -54);
+          expect(f1_scale).toBeWithinRange(0.544, 0.546);
+          expect(f1_rotation).toBe(0);
+          expect(f2_dimensions.left).toBeWithinRange(0, 0.5);
+          expect(f2_dimensions.top).toBeWithinRange(0, 0.5);
+          expect(f2_scale).toBeWithinRange(0.544, 0.546);
+          expect(f2_rotation).toBe(0);
+          expect(f3_dimensions.left).toBeWithinRange(-96, -93);
+          expect(f3_dimensions.top).toBeWithinRange(-50, -49);
+          expect(f3_scale).toBeWithinRange(0.272, 0.273);
+          expect(f3_rotation).toBe(30);
         });
       });
     });
@@ -91,66 +89,64 @@ describe("CanvasLayout", function() {
 
   it('will transition to a frame and will apply a transform on all frames within the layer', function() {
     browser.get('canvasLayout/canvaslayout.html').then(function() {
-      browser.sleep(3000).then(function() {
-        var stage = element(by.id('root'));
-        var layer1 = element(by.id('layer1'));
-        var f1 = element(by.id('main'));
-        var f2 = element(by.id('second'));
-        var f3 = element(by.id('thirth'));
+      var stage = element(by.id('root'));
+      var layer1 = element(by.id('layer1'));
+      var f1 = element(by.id('main'));
+      var f2 = element(by.id('second'));
+      var f3 = element(by.id('thirth'));
 
-        expect(f1.getCssValue('display')).toBe('block');
-        expect(f2.getCssValue('display')).toBe('block');
-        expect(f3.getCssValue('display')).toBe('block');
+      expect(f1.getCssValue('display')).toBe('block');
+      expect(f2.getCssValue('display')).toBe('block');
+      expect(f3.getCssValue('display')).toBe('block');
 
-        utilities.transitionTo('layer1', 'thirth', {}).then(function() {
+      utilities.transitionTo('layer1', 'thirth', {}).then(function() {
 
-          protractor.promise.all([
-            utilities.getBoundingClientRect('root'),
-            f1.getCssValue('display'),
-            utilities.getBoundingClientRect('main'),
-            utilities.getScale('main'),
-            utilities.getRotation('main'),
-            f2.getCssValue('display'),
-            utilities.getBoundingClientRect('second'),
-            utilities.getScale('second'),
-            utilities.getRotation('second'),
-            f3.getCssValue('display'),
-            utilities.getBoundingClientRect('thirth'),
-            utilities.getScale('thirth'),
-            utilities.getRotation('thirth')
-          ]).then(function(data) {
+        protractor.promise.all([
+          utilities.getBoundingClientRect('root'),
+          f1.getCssValue('display'),
+          utilities.getBoundingClientRect('main'),
+          utilities.getScale('main'),
+          utilities.getRotation('main'),
+          f2.getCssValue('display'),
+          utilities.getBoundingClientRect('second'),
+          utilities.getScale('second'),
+          utilities.getRotation('second'),
+          f3.getCssValue('display'),
+          utilities.getBoundingClientRect('thirth'),
+          utilities.getScale('thirth'),
+          utilities.getRotation('thirth')
+        ]).then(function(data) {
 
-            var stage_dimensions = data[0];
-            var f1_display = data[1];
-            var f1_dimensions = data[2];
-            var f1_scale = data[3];
-            var f1_rotation = data[4];
-            var f2_display = data[5];
-            var f2_dimensions = data[6];
-            var f2_scale = data[7];
-            var f2_rotation = data[8];
-            var f3_display = data[9];
-            var f3_dimensions = data[10];
-            var f3_scale = data[11];
-            var f3_rotation = data[12];
+          var stage_dimensions = data[0];
+          var f1_display = data[1];
+          var f1_dimensions = data[2];
+          var f1_scale = data[3];
+          var f1_rotation = data[4];
+          var f2_display = data[5];
+          var f2_dimensions = data[6];
+          var f2_scale = data[7];
+          var f2_rotation = data[8];
+          var f3_display = data[9];
+          var f3_dimensions = data[10];
+          var f3_scale = data[11];
+          var f3_rotation = data[12];
 
-            expect(f1_display).toBe('block');
-            expect(f2_display).toBe('block');
-            expect(f3_display).toBe('block');
-            //expect(stage_dimensions.width).toBe(f3_dimensions.width);
-            expect(f1_dimensions.left).toBeWithinRange(-169, -167);
-            expect(f1_dimensions.top).toBeWithinRange(-33, -31);
-            expect(f1_scale).toBeWithinRange(0.544, 0.546);
-            expect(f1_rotation).toBe(-30);
-            expect(f2_dimensions.left).toBeWithinRange(93, 96);
-            expect(f2_dimensions.top).toBeWithinRange(-392, -390);
-            expect(f2_scale).toBeWithinRange(0.544, 0.546);
-            expect(f2_rotation).toBe(-30);
-            expect(f3_dimensions.left).toBeWithinRange(-0.5, 0);
-            expect(f3_dimensions.top).toBeWithinRange(0, 0.5);
-            expect(f3_scale).toBeWithinRange(0.272, 0.273);
-            expect(f3_rotation).toBe(0);
-          });
+          expect(f1_display).toBe('block');
+          expect(f2_display).toBe('block');
+          expect(f3_display).toBe('block');
+          //expect(stage_dimensions.width).toBe(f3_dimensions.width);
+          expect(f1_dimensions.left).toBeWithinRange(-169, -167);
+          expect(f1_dimensions.top).toBeWithinRange(-33, -31);
+          expect(f1_scale).toBeWithinRange(0.544, 0.546);
+          expect(f1_rotation).toBe(-30);
+          expect(f2_dimensions.left).toBeWithinRange(93, 96);
+          expect(f2_dimensions.top).toBeWithinRange(-392, -390);
+          expect(f2_scale).toBeWithinRange(0.544, 0.546);
+          expect(f2_rotation).toBe(-30);
+          expect(f3_dimensions.left).toBeWithinRange(-0.5, 0);
+          expect(f3_dimensions.top).toBeWithinRange(0, 0.5);
+          expect(f3_scale).toBeWithinRange(0.272, 0.273);
+          expect(f3_rotation).toBe(0);
         });
       });
     });
---
+++ test/e2e/fittingfunctions/fitto_spec.js
@@ -0,0 +1,355 @@
+var utilities = require('../helpers/utilities.js');
+
+describe("Fit to", function() {
+
+  var baseFolder = "fittingfunctions/";
+
+  beforeEach(function() {
+    // window size can be set
+    browser.driver.manage().window().setSize(800, 600);
+  });
+
+
+  it("width", function() {
+    browser.get(baseFolder + "fitto_width.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.width).toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).toBe(frame_dimensions.width);
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+
+        expect(frame_scale).not.toBe(1);
+      });
+    });
+  });
+
+  it("height", function() {
+    browser.get(baseFolder + "fitto_height.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).toBe(frame_dimensions.height);
+
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+
+        expect(frame_scale).not.toBe(1);
+      });
+    });
+  });
+
+  it("fixed", function() {
+    browser.get(baseFolder + "fitto_fixed.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(frame_dimensions.width).toBe(1000);
+        expect(frame_dimensions.height).toBe(1000);
+        expect(frame_scale).toBe(1);
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+      });
+    });
+  });
+
+  it("contain", function() {
+    browser.get(baseFolder + "fitto_contain.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(frame_dimensions.height <= stage_dimensions.height).toBeTruthy();
+        expect(frame_dimensions.width <= layer_dimensions.width).toBeTruthy();
+
+        expect(frame_scale).not.toBe(1);
+      });
+    });
+  });
+
+  it("cover with X scroller", function() {
+    browser.get(baseFolder + "fitto_cover_scrollX.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).toBe(frame_dimensions.height);
+        expect(stage_dimensions.height < frame_dimensions.width).toBeTruthy();
+        expect(frame_dimensions.width < 1500).toBeTruthy();
+        expect(frame_scale).not.toBe(1);
+      });
+    });
+  });
+
+  it("cover with Y scroller", function() {
+    browser.get(baseFolder + "fitto_cover_scrollY.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.width).toBe(frame_dimensions.width);
+        expect(stage_dimensions.height < frame_dimensions.height).toBeTruthy();
+        expect(frame_dimensions.height < 1500).toBeTruthy();
+        expect(frame_scale).not.toBe(1);
+      });
+    });
+  });
+
+  it("responsive", function() {
+    browser.get(baseFolder + "fitto_responsive.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).toBe(frame_dimensions.width);
+        expect(frame_scale).toBe(1);
+      });
+    });
+  });
+
+  it("responsive-width", function() {
+    browser.get(baseFolder + "fitto_responsive-width.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).toBe(frame_dimensions.width);
+        expect(frame_dimensions.height).toBe(1000);
+        expect(frame_scale).toBe(1);
+      });
+    });
+  });
+
+  it("responsive-height", function() {
+    browser.get(baseFolder + "fitto_responsive-height.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(frame_dimensions.width).toBe(1500);
+        expect(frame_scale).toBe(1);
+      });
+    });
+  });
+
+  it("elastic-width with shiftX", function() {
+    browser.get(baseFolder + "fitto_elastic-width-shiftX.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(frame_scale).toBe(1);
+        expect(frame_dimensions.left).toBeWithinRange(-1, 0);
+      });
+    });
+  });
+
+  it("elastic-width with scale", function() {
+    browser.get(baseFolder + "fitto_elastic-width-scale.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).toBe(frame_dimensions.width);
+        expect(frame_scale > 1).toBeTruthy();
+        expect(frame_dimensions.left).toBe(0);
+      });
+    });
+  });
+
+  it("elastic-width with scale and shiftX", function() {
+    browser.get(baseFolder + "fitto_elastic-width-scale_and_shiftX.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(frame_dimensions.width).toBe(625);
+        expect(frame_scale > 1).toBeTruthy();
+        expect(frame_dimensions.left).toBe(-50);
+      });
+    });
+  });
+
+  it("elastic-height with shiftY", function() {
+    browser.get(baseFolder + "fitto_elastic-height-shiftY.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(frame_scale).toBe(1);
+        expect(frame_dimensions.top).toBeWithinRange(-1, 0);
+      });
+    });
+  });
+
+  it("elastic-height with scale", function() {
+    browser.get(baseFolder + "fitto_elastic-height-scale.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(stage_dimensions.height).toBe(frame_dimensions.height);
+        expect(layer_dimensions.height).toBe(frame_dimensions.height);
+        expect(frame_scale > 1).toBeTruthy();
+        expect(frame_dimensions.top).toBe(0);
+      });
+    });
+  });
+
+  it("elastic-height with scale and shiftY", function() {
+    browser.get(baseFolder + "fitto_elastic-height-scale_and_shiftY.html").then(function() {
+      protractor.promise.all([
+        utilities.getBoundingClientRect("stage"),
+        utilities.getBoundingClientRect("layer"),
+        utilities.getBoundingClientRect("frame"),
+        utilities.getScale("frame")
+      ]).then(function(data) {
+        var stage_dimensions = data[0];
+        var layer_dimensions = data[1];
+        var frame_dimensions = data[2];
+        var frame_scale = data[3];
+
+        expect(stage_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(layer_dimensions.width).not.toBe(frame_dimensions.width);
+        expect(stage_dimensions.height).not.toBe(frame_dimensions.height);
+        expect(frame_dimensions.height).toBe(625);
+        expect(frame_scale > 1).toBeTruthy();
+        expect(frame_dimensions.top).toBe(-50);
+      });
+    });
+  });
+});
