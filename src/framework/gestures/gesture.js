656f5f6465f729a5c77a9f252e8ff5e35e96d628
ktordeur <kenny.tordeur@outlook.com>
2016-04-08 12:18:01 +0200

    gesture (define gesture class)

---
+++ src/framework/gestures/gesture.js
@@ -0,0 +1,41 @@
+var gesture = function() {
+
+  this.altKey = false;
+  this.buttons = [false, false, false];
+  this.cancel = false;
+  this.click = false;
+  //this.ctrlKey = false;
+  //this.dbl = false;
+  this.doubleClick = false;
+  this.event = null;
+  this.first = false;
+  this.id = 0;
+  this.last = false;
+  //this.lng = false;
+  //this.longClick = false;
+  this.move = false;
+  this.multi = false;
+  //this.rotation = 0;
+  //this.scale = 1;
+
+  this.shift = {
+    x: 0,
+    x: 1
+  };
+  this.shiftKey = false;
+  this.start = {
+    x: 0,
+    y: 0
+  };
+  this.startTime = new Date().getTime();
+  this.touch = false;
+  //this.transform = false;
+  this.wheel = false;
+  this.wheelDelta = 0;
+  this.position = {
+    x: 0,
+    y: 0
+  };
+};
+
+module.exports = gesture;
