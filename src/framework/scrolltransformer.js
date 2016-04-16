098279e6718b30b0e4c8c9b08245b033d9169add
Thomas Handorf <thomas@webpgr.com>
2016-04-16 22:32:44 +0200

    layertransform: fix frametransformdata did not return scroll data in frame space (but in stage space) + transform scrolling applied in wrong direction
    --HG--
    branch : transform scrolling

--- src/framework/scrolltransformer.js
+++ src/framework/scrolltransformer.js
@@ -102,7 +102,7 @@ var ScrollTransformer = Kern.EventManager.extend({
       this.layer.innerEl.style.height = 0;
       this.layer.innerEl.style.width = 0;
       // in transformscroll we add a transform representing the scroll position.
-      return this.scrollTransform((scrollX || tfd.scrollX) * tfd.scale, (scrollY || tfd.scrollY) * tfd.scale);
+      return this.scrollTransform(-(scrollX || tfd.scrollX) * tfd.scale, -(scrollY || tfd.scrollY) * tfd.scale);
     }
   }
 });
