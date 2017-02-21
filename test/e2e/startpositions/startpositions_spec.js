8fa1299929b1d2b7e3a432015ac9a50d32be5d32
Kenny Tordeur <kenny.tordeur@outlook.com>
2017-02-21 16:20:57 +0100

    tests for startPosition

--- test/e2e/startpositions/startpositions_spec.js
+++ test/e2e/startpositions/startpositions_spec.js
@@ -102,6 +102,202 @@ describe('Start Positions', function() {
         });
       });
     });
+
+
+    describe('with fit-to=fixed', function() {
+
+      describe('frame size < stage size', function() {
+
+        beforeEach(function(done) {
+          browser.get('startpositions/nativescrolling_true/fitto_fixed/startposition_200x200.html').then(function() {
+            utilities.resizeWindow(600, 600);
+            done();
+          });
+        });
+
+        it('start-position=top-left', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'top-left').then(function() {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer'),
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(frame_dimensions.top).toBe(0);
+              expect(frame_dimensions.left).toBe(0);
+            });
+          });
+        });
+
+        it('start-position=top-right', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'top-right').then(function() {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(frame_dimensions.top).toBe(0);
+              expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+            });
+          });
+        });
+
+        it('start-position=bottom-left', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-left').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(frame_dimensions.left).toBe(0);
+              expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+            });
+          });
+        });
+
+        it('start-position=bottom-right', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-right').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+              expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+            });
+          });
+        });
+
+        it('start-position=middle-center', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'middle-center').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(frame_dimensions.left).toBe(layer_dimensions.width / 2 - frame_dimensions.width / 2);
+              expect(frame_dimensions.top).toBe(layer_dimensions.height / 2 - frame_dimensions.height / 2);
+            });
+          });
+        });
+      });
+
+      describe('frame size > stage size', function() {
+
+        beforeEach(function(done) {
+          browser.get('startpositions/nativescrolling_true/fitto_fixed/startposition_700x700.html').then(function() {
+            utilities.resizeWindow(600, 600);
+            done();
+          });
+        });
+
+        it('start-position=top-left', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'top-left').then(function() {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer'),
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(layer_scroll.scrollTop).toBe(0);
+            });
+          });
+        });
+
+        it('start-position=top-right', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'top-right').then(function() {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollTop).toBe(0);
+              expect(layer_scroll.scrollLeft).toBe(Math.abs(layer_dimensions.width - frame_dimensions.width));
+            });
+          });
+        });
+
+        it('start-position=bottom-left', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-left').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollLeft).toBe(0);
+              expect(layer_scroll.scrollTop).toBe(Math.abs(layer_dimensions.height - frame_dimensions.height));
+            });
+          });
+        });
+
+        it('start-position=bottom-right', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-right').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollLeft).toBe(Math.abs(layer_dimensions.width - frame_dimensions.width));
+              expect(layer_scroll.scrollTop).toBe(Math.abs(layer_dimensions.height - frame_dimensions.height));
+            });
+          });
+        });
+
+        it('start-position=middle-center', function() {
+          utilities.setAttribute('frame', 'data-lj-start-position', 'middle-center').then(function(data) {
+            protractor.promise.all([
+              utilities.getBoundingClientRect('frame'),
+              utilities.getBoundingClientRect('layer'),
+              utilities.getScroll('layer')
+            ]).then(function(data) {
+              var frame_dimensions = data[0];
+              var layer_dimensions = data[1];
+              var layer_scroll = data[2];
+              expect(layer_scroll.scrollLeft).toBe(Math.abs(layer_dimensions.width / 2 - frame_dimensions.width / 2));
+              expect(layer_scroll.scrollTop).toBe(Math.abs(layer_dimensions.height / 2 - frame_dimensions.height / 2));
+            });
+          });
+        });
+
+
+      });
+    });
   });
 
 
@@ -143,7 +339,7 @@ describe('Start Positions', function() {
           ]).then(function(data) {
             var stage_dimensions = data[0];
             var frame_dimensions = data[1];
-            
+
             expect(frame_dimensions.left).toBe((stage_dimensions.width - frame_dimensions.width));
           });
         });
@@ -193,6 +389,157 @@ describe('Start Positions', function() {
         });
       });
     });
+
+    describe('with fit-to=fixed', function() {
+
+     describe('frame size < stage size', function() {
+
+       beforeEach(function(done) {
+         browser.get('startpositions/nativescrolling_false/fitto_fixed/startposition_200x200.html').then(function() {
+           done();
+         });
+       });
+
+       it('start-position=top-left', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'top-left').then(function(data) {
+           utilities.getBoundingClientRect('frame').then(function(frame_dimensions) {
+             expect(frame_dimensions.left).toBe(0);
+             expect(frame_dimensions.top).toBe(0);
+           });
+         });
+       });
+
+       it('start-position=top-right', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'top-right').then(function() {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'),
+             utilities.getBoundingClientRect('layer'),
+             element(by.id('frame')).getAttribute('data-lj-start-position')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.top).toBe(0);
+             expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+           });
+         });
+
+       });
+
+       it('start-position=bottom-left', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-left').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(0);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+           });
+         });
+       });
+
+       it('start-position=bottom-right', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-right').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+           });
+         });
+       });
+
+       it('start-position=middle-center', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'middle-center').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(layer_dimensions.width / 2 - frame_dimensions.width / 2);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height / 2 - frame_dimensions.height / 2);
+           });
+         });
+       });
+     });
+
+     describe('frame size > stage size', function() {
+
+       beforeEach(function(done) {
+         browser.get('startpositions/nativescrolling_false/fitto_fixed/startposition_700x700.html').then(function() {
+           done();
+         });
+       });
+
+
+       it('start-position=top-left', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'top-left').then(function(data) {
+           utilities.getBoundingClientRect('frame').then(function(frame_dimensions) {
+             expect(frame_dimensions.left).toBe(0);
+             expect(frame_dimensions.top).toBe(0);
+           });
+         });
+       });
+
+       it('start-position=top-right', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'top-right').then(function() {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'),
+             utilities.getBoundingClientRect('layer'),
+             element(by.id('frame')).getAttribute('data-lj-start-position')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.top).toBe(0);
+             expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+           });
+         });
+
+       });
+
+       it('start-position=bottom-left', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-left').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(0);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+           });
+         });
+       });
+
+       it('start-position=bottom-right', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'bottom-right').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(layer_dimensions.width - frame_dimensions.width);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height - frame_dimensions.height);
+           });
+         });
+       });
+
+       it('start-position=middle-center', function() {
+         utilities.setAttribute('frame', 'data-lj-start-position', 'middle-center').then(function(data) {
+           protractor.promise.all([
+             utilities.getBoundingClientRect('frame'), utilities.getBoundingClientRect('layer')
+           ]).then(function(data) {
+             var frame_dimensions = data[0];
+             var layer_dimensions = data[1];
+             expect(frame_dimensions.left).toBe(layer_dimensions.width / 2 - frame_dimensions.width / 2);
+             expect(frame_dimensions.top).toBe(layer_dimensions.height / 2 - frame_dimensions.height / 2);
+           });
+         });
+       });
+     });
+   });
+ });
   });
 
 });
