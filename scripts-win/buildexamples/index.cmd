97dfc9780abbb16757a82c369edeff913d52ed4e
Thomas Handorf <thomas@layerjs.com>
2017-03-02 17:42:36 +0100

    package: moved distribution layerjs.css from ./dist/css/ to ./dist/; updated how examples are build (they don’t need to be build except they should be deployed on website)

--- scripts-win/buildexamples/index.cmd
+++
@@ -1,3 +0,0 @@
-For /R examples %%G in (layerjs.js) do ( if exist "%%G" copy dist\layerjs.js "%%G")
-
-For /R examples %%G in (layerjs.css) do (if exist "%%G" copy dist\css\layerjs.css "%%G")
--- scripts-win/package/index.cmd
+++ scripts-win/package/index.cmd
@@ -1,6 +1,5 @@
 mkdir dist
 copy /Y build\layerjs.js dist\layerjs.js
 copy /Y build\layerjs.js dist\layerjs-%npm_package_version%.js
-mkdir dist\css
-copy /Y src\css\layerjs.css dist\css\layerjs.css
-copy /Y src\css\layerjs.css dist\css\layerjs-%npm_package_version%.css
+copy /Y src\css\layerjs.css dist\layerjs.css
+copy /Y src\css\layerjs.css dist\layerjs-%npm_package_version%.css
