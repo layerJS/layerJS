9a8bf51763dc272de51b6a48093849147accb409
Kenny Tordeur <kenny.tordeur@outlook.com>
2017-03-13 23:10:37 +0100

    string.startsWith: IE fix backported

--- src/framework/layerjs.js
+++ src/framework/layerjs.js
@@ -1,4 +1,5 @@
 var $ = require('./domhelpers.js');
+require('./polyfill.js');
 // this module defines a global namespace for all weblayer objects.
 layerJS = {
   select: $.selectView,
---
+++ src/framework/polyfill.js
@@ -0,0 +1,24 @@
+'use strict';
+
+if (!String.prototype.endsWith) {
+  Object.defineProperty(String.prototype, 'endsWith', {
+    value: function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.lastIndexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+    }
+  });
+}
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    value: function(searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+    }
+  });
+}
