a7015739c5e9818fe5c1bf9bc134dc9989ae4b25
Kenny Tordeur <kenny.tordeur@outlook.com>
2017-09-06 15:35:57 +0200

    interstage tests

--- test/e2e/interstage/interstage_spec.js
+++ test/e2e/interstage/interstage_spec.js
@@ -7,21 +7,39 @@ describe('inter stage', function() {
     });
   });
 
+  function validate() {
+    var frameX = element(by.id('frameX'));
+    var stage1 = frameX.element('ancestor::[@id="stage1"][1]');
+    expect(stage1).toBeDefined();
+    protractor.promise.all([
+        browser.getCurrentUrl(),
+        utilities.getBoundingClientRect('layer1'),
+        utilities.getBoundingClientRect('frameX')
+      ])
+      .then(function(data) {
+        var url = data[0];
+        var stage1_dimensions = data[1];
+        var frameX_dimensions = data[2];
+        url = url.split('/').pop();
+        expect(url).toBe('index.html#frameX;layer2.!none');
+
+        expect(stage1_dimensions.bottom).toBe(frameX_dimensions.bottom);
+        expect(stage1_dimensions.height).toBe(frameX_dimensions.height);
+        expect(stage1_dimensions.left).toBe(frameX_dimensions.left);
+        expect(stage1_dimensions.right).toBe(frameX_dimensions.right);
+        expect(stage1_dimensions.opacity).toBe(frameX_dimensions.opacity);
+        expect(stage1_dimensions.top).toBe(frameX_dimensions.top);
+        expect(stage1_dimensions.width).toBe(frameX_dimensions.width);
+      });
+  }
+
   it('do a simple transition', function() {
     var frameX = element(by.id('frameX'));
     var stage2 = frameX.element('ancestor::[@id="stage2"][1]');
     expect(stage2).toBeDefined();
-
     utilities.transitionTo('layer1', 'frameX', {})
       .then(function() {
-        var stage1 = frameX.element('ancestor::[@id="stage1"][1]');
-        expect(stage1).toBeDefined();
-        browser.getCurrentUrl().then(function(url) {
-          url = url.split('/').pop();
-          expect(url).toBe('index.html#frameX;layer2.!none');
-          expect(element(by.id('frame1')).isDisplayed()).toBeFalsy();
-          expect(element(by.id('frameX')).isDisplayed()).toBeTruthy();
-        });
+        validate();
       });
   });
 
@@ -32,14 +50,7 @@ describe('inter stage', function() {
 
     utilities.showFrame('layer1', 'frameX', {})
       .then(function() {
-        var stage1 = frameX.element('ancestor::[@id="stage1"][1]');
-        expect(stage1).toBeDefined();
-        browser.getCurrentUrl().then(function(url) {
-          url = url.split('/').pop();
-          expect(url).toBe('index.html#frameX;layer2.!none');
-          expect(element(by.id('frame1')).isDisplayed()).toBeFalsy();
-          expect(element(by.id('frameX')).isDisplayed()).toBeTruthy();
-        });
+        validate();
       });
   });
 });
