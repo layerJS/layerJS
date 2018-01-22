bf31d92697307c60198dc7109bb20c20f07b6826
Thomas Handorf <thomas@layerjs.com>
2018-01-22 20:37:54 +0100

    fixed build: prepending info to layerjs removed double \\ from strings

--- scripts/build/scripts.cmd
+++ scripts/build/scripts.cmd
@@ -1,3 +1,5 @@
 mkdir -p build
 browserify -e src/layerjs.js -o build/layerjs.js
-echo "$(bash scripts/info/index)\n$(cat build/layerjs.js)" > build/layerjs.js
+echo "$(bash scripts/info/index)\n" > build/info;
+concat -o build/layerjs.js.tmp build/info build/layerjs.js
+mv build/layerjs.js.tmp build/layerjs.js
