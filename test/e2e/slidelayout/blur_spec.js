1c80fa340d595486e7e08ef08bf5171d86ca7de4
Kenny Tordeur <kenny.tordeur@outlook.com>
2018-06-29 15:14:36 +0200

    longer transition time

--- test/e2e/slidelayout/blur_spec.js
+++ test/e2e/slidelayout/blur_spec.js
@@ -29,57 +29,60 @@ describe('blur', function() {
           'lj-transition': 'blur'
         })]).then(function() {
           // time for the style changes to take effect
-          utilities.wait(100).then(function() {
+          utilities.wait(500).then(function() {
+            protractor.promise.all([
+              utilities.listenDimensionsBeforeTransition('layer', 'frame1'),
+              utilities.listenDimensionsBeforeTransition('layer', 'frame2')
+            ]).then(function() {
+              utilities.transitionTo('layer', 'frame2', {
+                duration: '5s'
+              }).then(function() {
 
-            utilities.listenDimensionsBeforeTransition('layer', 'frame1');
-            utilities.listenDimensionsBeforeTransition('layer', 'frame2');
-            utilities.transitionTo('layer', 'frame2', {
-              duration: '0.3s'
-            }).then(function() {
-              protractor.promise.all([
-                utilities.getBoundingClientRect('stage'),
-                utilities.getBoundingClientRect('frame1'),
-                utilities.getBoundingClientRect('frame2'),
-                utilities.getFromStore('frame1'),
-                utilities.getFromStore('frame2')
-              ]).then(function(data) {
-                var stage_dimensions = data[0];
-                var frame1_dimensions_after = data[1];
-                var frame2_dimensions_after = data[2];
-                var frame1_dimensions_before = data[3];
-                var frame2_dimensions_before = data[4];
-                // frame1_display_after == none
-                expect(f1.getCssValue('display')).toBe('none');
-                // frame2_display_after == block
-                expect(f2.getCssValue('display')).toBe('block');
-                // opacity
-                expect(parseFloat(frame1_dimensions_before.opacity)).toBeWithinRange(0.9, 1);
-                expect(parseFloat(frame2_dimensions_before.opacity)).toBeWithinRange(0, 0.1);
-                expect(frame2_dimensions_after.opacity).toBe('1');
-                expect(frame1_dimensions_after.opacity).toBe('0');
-                // z-index shouldn't change by the transition
-                // expect(frame1_dimensions_before['z-index']).toBe('1'); // current code doesn't enforce z-index on current frame
-                expect(frame2_dimensions_before['z-index']).toBe('-1');
-                expect(frame2_dimensions_after['z-index']).toBe('auto');
-                expect(frame1_dimensions_after['z-index']).toBe('auto');
-                // positioning and dimensions of frame1 and frame 2
-                expect(frame1_dimensions_after.width).toBe(stage_dimensions.width);
-                expect(frame2_dimensions_after.width).toBe(stage_dimensions.width);
-                expect(frame1_dimensions_after.height).toBe(stage_dimensions.height);
-                expect(frame2_dimensions_after.height).toBe(stage_dimensions.height);
+                protractor.promise.all([
+                  utilities.getBoundingClientRect('stage'),
+                  utilities.getBoundingClientRect('frame1'),
+                  utilities.getBoundingClientRect('frame2'),
+                  utilities.getFromStore('frame1'),
+                  utilities.getFromStore('frame2')
+                ]).then(function(data) {
+                  var stage_dimensions = data[0];
+                  var frame1_dimensions_after = data[1];
+                  var frame2_dimensions_after = data[2];
+                  var frame1_dimensions_before = data[3];
+                  var frame2_dimensions_before = data[4];
+                  // frame1_display_after == none
+                  expect(f1.getCssValue('display')).toBe('none');
+                  // frame2_display_after == block
+                  expect(f2.getCssValue('display')).toBe('block');
+                  // opacity
+                  expect(parseFloat(frame1_dimensions_before.opacity)).toBeWithinRange(0.9, 1);
+                  expect(parseFloat(frame2_dimensions_before.opacity)).toBeWithinRange(0, 0.1);
+                  expect(frame2_dimensions_after.opacity).toBe('1');
+                  expect(frame1_dimensions_after.opacity).toBe('0');
+                  // z-index shouldn't change by the transition
+                  // expect(frame1_dimensions_before['z-index']).toBe('1'); // current code doesn't enforce z-index on current frame
+                  expect(frame2_dimensions_before['z-index']).toBe('-1');
+                  expect(frame2_dimensions_after['z-index']).toBe('auto');
+                  expect(frame1_dimensions_after['z-index']).toBe('auto');
+                  // positioning and dimensions of frame1 and frame 2
+                  expect(frame1_dimensions_after.width).toBe(stage_dimensions.width);
+                  expect(frame2_dimensions_after.width).toBe(stage_dimensions.width);
+                  expect(frame1_dimensions_after.height).toBe(stage_dimensions.height);
+                  expect(frame2_dimensions_after.height).toBe(stage_dimensions.height);
 
-                delete frame1_dimensions_before.opacity;
-                delete frame2_dimensions_before.opacity;
-                delete frame1_dimensions_after.opacity;
-                delete frame2_dimensions_after.opacity;
-                delete frame1_dimensions_before['z-index'];
-                delete frame1_dimensions_after['z-index'];
-                delete frame2_dimensions_before['z-index'];
-                delete frame2_dimensions_after['z-index'];
+                  delete frame1_dimensions_before.opacity;
+                  delete frame2_dimensions_before.opacity;
+                  delete frame1_dimensions_after.opacity;
+                  delete frame2_dimensions_after.opacity;
+                  delete frame1_dimensions_before['z-index'];
+                  delete frame1_dimensions_after['z-index'];
+                  delete frame2_dimensions_before['z-index'];
+                  delete frame2_dimensions_after['z-index'];
 
-                expect(frame1_dimensions_before).toEqual(frame2_dimensions_before);
-                expect(frame2_dimensions_before).toEqual(frame1_dimensions_after);
-                expect(frame1_dimensions_before).toEqual(frame2_dimensions_after);
+                  expect(frame1_dimensions_before).toEqual(frame2_dimensions_before);
+                  expect(frame2_dimensions_before).toEqual(frame1_dimensions_after);
+                  expect(frame1_dimensions_before).toEqual(frame2_dimensions_after);
+                });
               });
             });
           });
